3dc7c1ea0110837e1880707171f3c40474519db1
Vaishnavi <vaishnavip2405@gmail.com>
2025-11-05 20:02:45 +0530

    added .gitignore file,.env file, installed nodemon

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const express = require("express");
 const app = express();
 
 const PORT = 8080;
-app.get('/test',(req,res)=>{
+app.get('/home',(req,res)=>{
 res.end("hello from Express")
 })
 app.listen(PORT, () => {
-  console.log(`express server lisning on ${PORT}`);
+  console.log(`express server lisning on port ${PORT}`);
 });
+
+
+
+
