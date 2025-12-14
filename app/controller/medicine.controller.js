58af884f0848b03a472654336e6fe879aade48ab
Vaishnavi <vaishnavip2405@gmail.com>
2025-12-14 17:23:07 +0530

    updated api

--- app/controller/medicine.controller.js
+++ app/controller/medicine.controller.js
@@ -1,24 +1,38 @@
 const constant = require("../../constant");
-const { saveMedicineValidaton } = require("../middelware/validationWithJOI");
-const { saveMedicines } = require("../service/medicine.service");
+const {
+  saveMedicineValidaton,
+  getmedicinesByIdValidation,
+} = require("../middelware/validationWithJOI");
+const {
+  saveMedicines,
+  getmedicinesById,
+  deleteMedicinesById,
+  updateMedicineNames,
+} = require("../service/medicine.service");
 
 exports.addMedicines = async (req, res) => {
-  
   //added joi library for validation for validation
   const { error } = saveMedicineValidaton.validate(req.body);
 
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
-  const { medicineName, strip, categories, quantityReceived, expiryDate } =
-    req.body;
+  const {
+    medicineName,
+    strip,
+    categories,
+    quantityReceived,
+    expiryDate,
+    medicineId,
+  } = req.body;
 
   const saveData = await saveMedicines(
     medicineName,
     strip,
     categories,
     quantityReceived,
-    expiryDate
+    expiryDate,
+    medicineId
   );
   if (!saveData) {
     return res.status(constant.STATUSCODES.BAD_REQUEST).json({
@@ -29,5 +43,62 @@ exports.addMedicines = async (req, res) => {
   return res.status(constant.STATUSCODES.ok).json({
     message: "Medicines added successfully",
     data: saveData,
+  });
+};
+
+exports.getMedicineById = async (req, res) => {
+  const id = req.query.medicineId;
+  console.log("id", id);
+
+  const response = await getmedicinesById(id);
+  console.log("response", response);
+  return res.status(constant.STATUSCODES.ok).json({
+    message: "Medicines fetched successfully",
+    response,
+  });
+};
+
+exports.deleteMedicines = async (req, res) => {
+  const id = req.query.medicineId;
+  console.log("id", id);
+
+  if (!id) {
+    return res.status(constant.STATUSCODES.BAD_REQUEST).json({
+      message: "MedicineId is required",
+    });
+  }
+  const response = await deleteMedicinesById(id);
+  console.log("response", response);
+  if (!response) {
+    return res.status(constant.STATUSCODES.BAD_REQUEST).json({
+      message: "Failed to delete medicines",
+    });
+  }
+  console.log("response", response);
+  return res.status(constant.STATUSCODES.ok).json({
+    message: "Medicines fetched successfully",
+    response,
+  });
+};
+
+exports.updateMedicines = async (req, res) => {
+  const { medicineId, medicineName } = req.body || {};
+  console.log("called api");
+  if (!medicineId || !medicineName) {
+    return res.status(constant.STATUSCODES.BAD_REQUEST).json({
+      message: "All filds are required",
+    });
+  }
+  const response = await updateMedicineNames(medicineId, medicineName);
+
+  if (!response) {
+    return res.status(constant.STATUSCODES.BAD_REQUEST).json({
+      message: "Failed to update names ",
+    });
+  }
+
+  return res.status(constant.STATUSCODES.ok).json({
+    message: " Updated sucessfully",
+    response,
   });
 };
